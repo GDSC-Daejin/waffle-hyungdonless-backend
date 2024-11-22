387a75b089c458118b560da210ef8039365e9468
codebox <codebox@codeboxui-MacBookAir.local>
2024-11-22 18:50:46 +0900

    초기설정

---
+++ src/main/java/com/example/waffle_project/WaffleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.waffle_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaffleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaffleProjectApplication.class, args);
+	}
+
+}
