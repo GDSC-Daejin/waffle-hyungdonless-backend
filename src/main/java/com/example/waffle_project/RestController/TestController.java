4d99e12f62ccbc63ffe21e6ceff8178f7b9f2be6
june <0721hjp@naver.com>
2025-01-22 11:47:59 +0900

    docs: 스웨거 설명 추가 - 유저 관련 api 설명 추가함

--- src/main/java/com/example/waffle_project/RestController/TestController.java
+++ src/main/java/com/example/waffle_project/RestController/TestController.java
@@ -1,11 +1,13 @@
 package com.example.waffle_project.RestController;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Test-Controller", description = "소셜 로그인을 위한 테스트 API")
 @RestController
 public class TestController {
 
--- src/main/java/com/example/waffle_project/RestController/UserController.java
+++ src/main/java/com/example/waffle_project/RestController/UserController.java
@@ -2,10 +2,13 @@ package com.example.waffle_project.RestController;
 
 import com.example.waffle_project.Dto.UserDto;
 import com.example.waffle_project.Service.Service;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "User-Controller", description = "회원 관리 API")
 @RestController
 @RequestMapping("/users")
 @CrossOrigin(origins = "*")
@@ -14,6 +17,7 @@ public class UserController {
     private Service service;
 
     //회원가입
+    @Operation(summary = "회원가입", description = "유저 정보를 등록하고 성공 시 userDto 반환")
     @PostMapping("/signup")
     public ResponseEntity<?> userSignUp(@RequestBody UserDto userDto){
         if(service.saveUserInfo(userDto)){
@@ -25,6 +29,7 @@ public class UserController {
     }
 
     //로그인
+    @Operation(summary = "로그인", description = "유저 정보를 조회하고 성공 시 token 반환")
     @PostMapping("/login")
     public ResponseEntity<?> userLogin(@RequestBody UserDto userDto){
         String token = service.userLogin(userDto); //로그인 성공 시 token 반환
@@ -36,6 +41,7 @@ public class UserController {
     }
 
     //특정회원조회
+    @Operation(summary = "특정회원조회", description = "특정 회원의 정보를 조회하고 성공 시 userDto 반환")
     @GetMapping("/{email}")
     public ResponseEntity<?> userFind(@PathVariable String email){ //특정 회원 조회
         UserDto userDto = service.userFind(email); //email로 회원정보 조회 / dto반환
@@ -48,6 +54,8 @@ public class UserController {
     }
 
     //특정회원정보업데이트
+    @Operation(summary = "특정회원정보업데이트", description = "특정 회원의 정보를 업데이트하고 성공 시 반영된 userDto 반환\n" +
+            "dto의 유저 email값을 기준으로 변경 작업을 수행함")
     @PutMapping("/{email}")
     public ResponseEntity<?> userUpdate(@PathVariable String email, @RequestBody UserDto userDto){
         UserDto userDto1 = service.userUpdate(userDto); //email로 회원정보 업데이트 / dto 반환
@@ -59,6 +67,7 @@ public class UserController {
     }
 
     //특정회원삭제
+    @Operation(summary = "특정회원삭제", description = "특정 회원의 정보를 삭제하고 성공 시 삭제된 userDto 반환")
     @DeleteMapping("/{email}")
     public ResponseEntity<?> userDelete(@PathVariable String email){
         UserDto userDto = service.userDelete(email); //email로 회원정보 삭제
@@ -70,6 +79,7 @@ public class UserController {
     }
 
     //모든회원조회
+    @Operation(summary = "모든회원조회", description = "모든 회원의 정보를 조회하고 성공 시 userDto 리스트 반환")
     @GetMapping
     public ResponseEntity<?> userFindAll(){
         if(service.userFindAll() == null){
